101d24ac938694a620ea19b1acf9eafb23302c21
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2023-08-26 22:01:18 -0500

    Update new-sccmultitool.sh

--- new-sccmultitool.sh
+++ new-sccmultitool.sh
@@ -507,8 +507,12 @@ EOF
 	echo -e ""
 	cd /home/$alias
 	find /home/$alias/.${coindir}/* ! -name "wallet.dat" ! -name "*.conf" -delete
-	echo -e "${YELLOW}Downloading/Copying and installing chain files for ${MAGENTA}$alias${NC}"
-
+	echo -e "${YELLOW}Downloading ano/or Unzipping chain files for ${MAGENTA}$alias${NC}"
+	echo -e ""
+	
+	mkdir /home/$alias/.${coindir}
+	chown '$alias:$alias' /home/$alias/.${coindir}
+	
 	if [[ $bootstrapchoice == yes ]] && [[ $chaindownload == 0 ]]
 		then
 			sccfile=~/${coinname}.zip
