55664cf8338ac1ce04ac029c18c0b376cb1adf28
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2023-08-27 21:45:04 -0500

    Update new-sccmultitool.sh

--- new-sccmultitool.sh
+++ new-sccmultitool.sh
@@ -1774,7 +1774,7 @@ case $start in
 			echo -e "${YELLOW}Press ${CYAN}Control-C${YELLOW} to abort at alias selection to quit${NC}"
 			echo -e ""
 
-			foundone=1
+			foundone=0
 
 			for i in $(ls /home/)
 				do
